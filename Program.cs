3e1cb55e54afab940c3c487a15b90b71712389d4
Flaie <flaie9@gmail.com>
2024-10-23 15:56:22 -0300

    Vizualizando o Smartphone

--- Program.cs
+++ Program.cs
@@ -5,12 +5,12 @@ Iphone IphoneNovo = new Iphone(numero: "84522-2545", modelo: "Pro Max", imei: "2
 Nokia NokiaNovo = new Nokia(numero: "84522-3232", modelo: "Nokia 15", imei: "212456787878", memoria: 64);
 
 
-Console.WriteLine($"Novo celular Nokia {NokiaNovo.Numero}");
+Console.WriteLine($"Novo celular Nokia - Número:{NokiaNovo.Numero}, Modelo:{NokiaNovo.GetModelo()}, IMEI:{NokiaNovo.GetImei()}, Memória:{NokiaNovo.GetMemoria()}");
 NokiaNovo.Ligar();
 NokiaNovo.ReceberLigacao();
 NokiaNovo.InstalarAplicativo("WhatsApp");
 Console.WriteLine("");
-Console.WriteLine($"Novo celular Iphone {IphoneNovo.Numero}");
+Console.WriteLine($"Novo celular Iphone Número:{IphoneNovo.Numero}, Modelo:{IphoneNovo.GetModelo()}, IMEI:{IphoneNovo.GetImei()}, Memória:{IphoneNovo.GetMemoria()}");
 IphoneNovo.Ligar();
 IphoneNovo.ReceberLigacao();
 IphoneNovo.InstalarAplicativo("Telegram");
