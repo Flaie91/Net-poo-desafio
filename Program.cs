4c0dddb311a958651130800f277aa34962590b68
Flaie <flaie9@gmail.com>
2024-10-23 14:33:11 -0300

    Implementando Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using Net_poo_desafio.Models;
 
+Iphone IphoneNovo = new Iphone(numero: "84522-2545", modelo: "Pro Max", imei: "212456787878", memoria: 64 );
 
+Nokia NokiaNovo = new Nokia(numero: "84522-3232", modelo: "Nokia 15", imei: "212456787878", memoria: 64);
+
+
+Console.WriteLine($"Novo celular Nokia {NokiaNovo.Numero}");
+NokiaNovo.Ligar();
+NokiaNovo.ReceberLigacao();
+NokiaNovo.InstalarAplicativo("WhatsApp");
+Console.WriteLine("");
+Console.WriteLine($"Novo celular Iphone {IphoneNovo.Numero}");
+IphoneNovo.Ligar();
+IphoneNovo.ReceberLigacao();
+IphoneNovo.InstalarAplicativo("Telegram");
